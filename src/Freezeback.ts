9cbbd895f5cdca2361c79294232a26be53b1c1e7
mornya <mornya78@gmail.com>
2022-06-30 11:13:19 +0900

    버그 수정

--- src/Freezeback.ts
+++ src/Freezeback.ts
@@ -19,7 +19,7 @@ let isInitialized: boolean = false;
 /**
  * initialize
  */
-export function initialize (option?: Option): void {
+export function initialize(option?: Option): void {
   if (!isInitialized && typeof window !== 'undefined') {
     isInitialized = true;
 
@@ -34,7 +34,7 @@ export function initialize (option?: Option): void {
  *
  * @param needsUnfreeze {boolean?}
  */
-export function destroy (needsUnfreeze: boolean | undefined = true): void {
+export function destroy(needsUnfreeze: boolean | undefined = true): void {
   if (isInitialized) {
     if (needsUnfreeze) {
       window.history.go(-dataStack.length);
@@ -53,7 +53,7 @@ export function destroy (needsUnfreeze: boolean | undefined = true): void {
  * @private
  * @param event {PopStateEvent}
  */
-function popStateHandler (event: PopStateEvent): void {
+function popStateHandler(event: PopStateEvent): void {
   if (isFrozen()) {
     const data = dataStack.pop(); // 마지막 스택의 콜백부터 수행
 
@@ -62,9 +62,7 @@ function popStateHandler (event: PopStateEvent): void {
         data.onBack(event.state);
       } else {
         // freeze 되어있으나 콜백이 없다면 뒤로가기 방지
-        const hash = defaultOption.useHash
-          ? `${window.location.pathname}#${data.key}`
-          : undefined;
+        const hash = defaultOption.useHash ? `${window.location.pathname}#${data.key}` : undefined;
 
         window.history.pushState({ key: data.key, state: 'block' }, document.title, hash);
       }
@@ -78,11 +76,9 @@ function popStateHandler (event: PopStateEvent): void {
  * @param key {string}
  * @param onBack {OnBackCallback?}
  */
-export function freeze (key: string, onBack?: OnBackCallback): void {
+export function freeze(key: string, onBack?: OnBackCallback): void {
   if (isInitialized) {
-    const hash = defaultOption.useHash
-      ? `${window.location.pathname}#${key}`
-      : undefined;
+    const hash = defaultOption.useHash ? `${window.location.pathname}#${key}` : undefined;
 
     window.history.pushState({ key, state: 'frozen' }, document.title, hash);
     dataStack.push({ key, onBack });
@@ -94,11 +90,16 @@ export function freeze (key: string, onBack?: OnBackCallback): void {
  *
  * @param key {string}
  */
-export function unfreeze (key: string): void {
+export function unfreeze(key: string): void {
   if (isInitialized) {
     // 해당 key로 등록된 콜백 강제 제거시 사용
     if (isFrozen()) {
       dataStack = dataStack.filter((data) => data.key !== key);
+    }
+    // pushState 제거
+    const data = window.history.state as Data;
+    if (data.key === key) {
+      window.history.back();
     }
   }
 }
@@ -109,9 +110,7 @@ export function unfreeze (key: string): void {
  * @param key {string?}
  * @returns {boolean}
  */
-export function isFrozen (key?: string): boolean {
-  const has = key
-    ? dataStack.length > 0 && dataStack.some(data => data.key === key)
-    : dataStack.length > 0;
+export function isFrozen(key?: string): boolean {
+  const has = key ? dataStack.length > 0 && dataStack.some((data) => data.key === key) : dataStack.length > 0;
   return isInitialized ? has : false;
 }
